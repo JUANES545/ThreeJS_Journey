43af802cdc4af370348d381a547587ac0410d18a
Juan Mejía <juanes.reich@gmail.com>
2021-10-05 22:12:25 -0500

    Random triangles

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -13,6 +13,24 @@ const scene = new THREE.Scene()
 
 // Object
 // const geometry = new THREE.BoxGeometry(1, 1, 1,2,2,2)
+const geometry = new THREE.Geometry()
+
+for(let i = 0; i<50; i++){
+    for (let j = 0; j<3; j++){
+        geometry.vertices.push(new THREE.Vector3(
+            Math.random,
+            Math.random(),
+            Math.random
+        ))
+    }
+    const verticesIndex = i * 3
+    geometry.faces.push(new THREE.Face3(
+        verticesIndex + 0,
+        verticesIndex +1,
+        verticesIndex +2
+    ))
+}
+
 const material = new THREE.MeshBasicMaterial({
     color: 0xff0000,
     wireframe:true //to see the internal structure
