4fbdf62420c82746d4224c5c7a8a8293eb5beafb
Juan Mejía <juanes.reich@gmail.com>
2021-10-26 20:19:05 -0500

    repeat all parts

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -30,6 +30,7 @@ const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 colorTexture.repeat.x = 2 //we created a custom texture size
 colorTexture.repeat.y = 3
 colorTexture.wrapS = THREE.RepeatWrapping
+colorTexture.wrapT = THREE.RepeatWrapping
 
 /**
  * Base
