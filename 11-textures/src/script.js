fd9a46edb9e8b0ed87d2b461a78d9fc2e580d5cd
Juan Mejía <juanes.reich@gmail.com>
2021-10-26 19:55:00 -0500

    New code to load textures on the onload function v2 with texture door

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -25,7 +25,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ map: texture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
